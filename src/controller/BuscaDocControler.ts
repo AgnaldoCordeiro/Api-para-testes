9d0bc3cf14598765876b6768db44d084ca01c6ac
AgnaldoCordeiro <agske13@gmail.com>
2022-06-23 17:08:00 -0300

    arruamndo conversao

--- src/controller/BuscaDocControler.ts
+++ src/controller/BuscaDocControler.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
 
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key: any, value: any) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
+
 export class BuscaDocControler{
   handle(req: Request, res: Response) {
     const user = {
@@ -9,12 +22,14 @@ export class BuscaDocControler{
 
     
     const doc = req.body.doc
-    const result = JSON.stringify(Object.fromEntries(doc));
-    console.log(doc, result);
 
     if (user.doc == req.body.doc) {
-      return res.json({mensagem: res.status(201).send(`${user.name}: https://stisolucao.ddns.com.br:8090/?doc=${result}`)})
-      
+      const result = JSON.stringify(Object.fromEntries(doc), getCircularReplacer());
+      return res.json(
+        { mensagem: res.status(201).send(`${user.name}: https://stisolucao.ddns.com.br:8090/?doc=${result}`+ console.log(doc + result)) },
+         
+      )
+     
     }
     return res.json({mensagem: res.status(403).send("erro na api")})
     
