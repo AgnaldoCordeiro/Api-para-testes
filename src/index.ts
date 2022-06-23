80c7c51f3b8b1ace9386de50affd812db4f3a36b
AgnaldoCordeiro <agske13@gmail.com>
2022-06-23 15:05:56 -0300

    adcionando improtacao cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors'
 import { router } from './routes'
 
 const api = express();
 api.use(cors());
-api.use(express.json())
-api.use(router)
+api.use(express.json({limit: '50mb'}));
+api.use(express.urlencoded({ extended: false, limit: '50mb' }));
+api.use(express.json());
+api.set("view engine", "ejs");
+api.use(router);
 api.listen(process.env.PORT || 3333, () => {
   console.log("Servidor on")
 })
