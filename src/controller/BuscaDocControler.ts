139ddb20aec8806f6f935119ee05b9b6a9cc7b49
AgnaldoCordeiro <agske13@gmail.com>
2022-06-23 16:32:46 -0300

    adiconando função de remover obk circular

--- src/controller/BuscaDocControler.ts
+++ src/controller/BuscaDocControler.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
 
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key: any, value: any) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
 
 export class BuscaDocControler{
   handle(req: Request, res: Response) {
@@ -7,10 +19,13 @@ export class BuscaDocControler{
       name: "Deu Certo",
       doc: "44907141815"
     }
+
+    
     const doc = req.body.doc
 
     if (user.doc == req.body.doc) {
-      return res.json({mensagem: res.status(201).send(`${user.name}: https://stisolucao.ddns.com.br:8090/?doc=${doc}`)})
+      const result = JSON.stringify(doc, getCircularReplacer());
+      return res.json({mensagem: res.status(201).send(`${user.name}: https://stisolucao.ddns.com.br:8090/?doc=${result}`)})
       
     }
     return res.json({mensagem: res.status(403).send("erro na api")})
