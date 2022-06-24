e6493f7da61624173110883ece21fd96eb1a5d9c
AgnaldoCordeiro <agske13@gmail.com>
2022-06-24 10:04:48 -0300

    arruamndo typeerro

--- src/controller/BuscaDocControler.ts
+++ src/controller/BuscaDocControler.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
+import Flatted from "flatted";
 
 const getCircularReplacer = () => {
   const seen = new WeakSet();
-  return (key: any, value: any) => {
+  return (key: any, value: object) => {
     if (typeof value === 'object' && value !== null) {
       if (seen.has(value)) {
         return;
@@ -12,24 +13,23 @@ const getCircularReplacer = () => {
     return value;
   };
 };
-
+ 
 export class BuscaDocControler{
   handle(req: Request, res: Response) {
-    const user = {
-      name: "Deu Certo",
-      doc: "44907141815"
-    }
+   
+      const name = "Deu Certo"
+      const cpf = "44907141815"
+   
 
     
-    const doc = req.body.doc
-
-    if (user.doc == req.body.doc) {
-      const result = JSON.stringify(Object.fromEntries(doc), getCircularReplacer());
-      return res.json(
-        { mensagem: res.status(201).send(`${user.name}: https://stisolucao.ddns.com.br:8090/?doc=${result}`+ console.log(doc + result)) },
+    const doc = req.body.doc;
+    if (cpf === doc) {
+      JSON.stringify(doc, getCircularReplacer());
+      console.log(doc)
+      return res.json(`${name}: https://stisolucao.ddns.com.br:8090/?doc=${cpf}`
          
       )
-     
+    
     }
     return res.json({mensagem: res.status(403).send("erro na api")})
     
